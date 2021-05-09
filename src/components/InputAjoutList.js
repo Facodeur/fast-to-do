3702313685e34fdc67652ab4b2a025387846ed21
wilfried <wilfried.odorico@gmail.com>
2021-05-09 17:37:45 +0200

    ajout des method enregistrement db

--- src/components/InputAjoutList.js
+++ src/components/InputAjoutList.js
@@ -1,26 +1,44 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import styled from "styled-components";
 import useForm from "../hooks/useForm";
+import { FirebaseContext } from "../firebase";
 
 const initialState = {
   titre: ""
 }
 
 const InputAjoutList = ({ className }) => {
-  const { handleChange, values, setValues } = useForm(initialState)
-  
+  const { handleChange, values, setValues } = useForm(initialState);
+  const { firebase, user } = useContext(FirebaseContext);
+  console.log(user.uid)
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(values.titre)
+    const liste = {
+      user: user.uid,
+      titre: values.titre
+    }
+    firebase.db.collection('fastodo').add(liste)
+
+    setValues({titre: ""})
+  }
+
   return (
     <div className={className}>
-      <input 
+      <form onSubmit={handleSubmit}>
+        <input 
         name="titre"
         type="text" 
         placeholder="Nouvelle liste"
         value={values.titre}
         onChange={handleChange}
       />
-      <button onClick={() => console.log(values.titre)}>
+      <button type="submit">
         <i className="fas fa-plus"></i>
       </button>
+      </form>
+      
     </div>
   );
 };
@@ -30,6 +48,7 @@ export default styled(InputAjoutList)`
   justify-content: space-around;
   align-items: center;
   margin-top: 20px;
+  margin-bottom: 20px;
 
   input {
     border-radius: 4px;
